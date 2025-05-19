552dc3e900becaad15169f36f48bb6a4b7ff0fb8
seb9i <sebastianp136@nycstudents.net>
2025-05-19 10:55:33 -0400

    Delete mygame directory

--- mygame/area_2d.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-@onready var projectile_caller = $ProjectileCaller2D
-									
-func _process(_delta):
-	if Input.is_action_just_pressed("Right"):
-		var ball = get_parent().get_node("Area2D")
-
-		var destination = get_global_mouse_position()
-		print(projectile_caller.request_projectile(0, ball.global_position, destination))
