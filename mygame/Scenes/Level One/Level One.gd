82b6e186841c7d4747c12709d9063fc8a4221424
seb9i <sebastianp136@nycstudents.net>
2025-05-28 10:59:17 -0400

    fixed horse gamemode

--- mygame/Scenes/Level One/Level One.gd
+++ mygame/Scenes/Level One/Level One.gd
@@ -1,19 +1,32 @@
 extends Node2D
 var horse_enabled = false
-var player_turns = 3
-var ai_turns = 3
+
 var player_one = true
 var ai = false
 var previous_position = null
-var player_one_made_shot = true
+
 var ai_turn = true
+
+
 
 signal shots
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Dialogic.start("Level_One")
+	Scoreboard.shot_made = false
 	Dialogic.signal_event.connect(_on_dialogic_signal)
-	Scoreboard.shot.connect(doThing)
+
+func swap_positions():
+		print("swap positions")
+		if player_one:
+			player_one = false
+			ai = true
+			Scoreboard.player_turns -= 1
+		else:
+			player_one = true
+			ai = false
+			Scoreboard.ai_turns -= 1
+
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -24,49 +37,72 @@ func _process(delta):
 		$Basketball.global_position = Vector2(mouse_position_x, mouse_position_y)
 
 func doThing():
-	if player_turns <= 0 or ai_turns <= 0:
-		return
-	if Scoreboard.shot_made == false:
-		if player_one:
-			player_one = false
-			ai = true
-			player_turns -= 1
-		else:
-			player_one = true
-			ai = false
-			ai_turns -= 1
-	else:
+
+	while Scoreboard.player_turns >= 0 or Scoreboard.ai_turns >= 0:
+		await get_tree().create_timer(.2).timeout
+	
+
+	
 		
 	
 		if player_one:
-			print("player one made the first shot")
+			activate_horse(true)
+			print("Awaiting shot")
+			await Scoreboard.shot
+			print("Shot the ball")
+			if (Scoreboard.shot_made == false):
+				swap_positions()
+				continue
+
+
 			$Basketball.allow_input = false
 			await get_tree().create_timer(1.5).timeout
-			var ai_shot = randf_range(70, 100)
-			if ai_shot > 60:
+			var ai_shot = randf_range(50, 100)
+			if ai_shot > 50:
 
 				$Basketball.toss_ball_parabola($Basketball.collision_shape2.global_position, 70, $Basketball)
 			else:
 
 
-				$Basketball.toss_ball_parabola($Basketball.collision_shape2.global_position, 70, $Basketball, ai_shot)
-			print("Player ONE Main turn")
+				$Basketball.toss_ball_parabola($Basketball.collision_shape2.global_position, 70, $Basketball, ai_shot)	
+			print("Awaiting ai shot")
+			await Scoreboard.shot
+			print("AI shot the ball")
+			if (Scoreboard.shot_made == false):
+				Scoreboard.ai_turns -= 1
+				print("ai down by one")
 				
+
 				
 		if ai:
-			print("ai made the first shot")
+
 			$Basketball.allow_input = false
 			await get_tree().create_timer(1.5).timeout
 			var ai_shot = randf_range(70, 100)
-			if ai_shot > 60:
+			if ai_shot > 95:
 
 				$Basketball.toss_ball_parabola($Basketball.collision_shape2.global_position, 70, $Basketball)
 			else:
 
-
 				$Basketball.toss_ball_parabola($Basketball.collision_shape2.global_position, 70, $Basketball, ai_shot)
-				print("shot didn't go in.")
-			print("AI Main Turn")
+			print("AI TURN- AI SHOT")
+			await Scoreboard.shot
+
+			if (Scoreboard.shot_made == false):
+				swap_positions()
+				continue
+			
+			print("AI TURN- PLAYER_SHOT")
+			await get_tree().create_timer(.1).timeout
+			await Scoreboard.shot
+			if (Scoreboard.shot_made == false):
+				Scoreboard.player_turns -= 1
+				print("player down by one")
+
+
+	await Scoreboard.shot
+	await get_tree().create_timer(.2).timeout
+	doThing()
 		
 func activate_horse(boolean = false):
 	if boolean:
@@ -81,8 +117,8 @@ func activate_horse(boolean = false):
 
 func _on_dialogic_signal(argument:String):
 	if argument == "Pick":
-		activate_horse(true)
 		Dialogic.paused = true
+		doThing()
 
 
 func _unhandled_input(event) -> void:
