5d6fe052d305d709dbe31cc17e83935f880fd611
seb9i <sebastianp136@nycstudents.net>
2025-06-02 10:56:44 -0400

    use timer node instead of delta for meter speed

--- mygame/Assets/green_meter.gd
+++ mygame/Assets/green_meter.gd
@@ -1,20 +1,30 @@
 extends Control
 @onready var meter = $TextureProgressBar
-var speed = 0.6
-var transparency = 0.01   
+var speed = 2
+var transparency = 0.02   
 @onready var basketball = get_parent().get_node("Basketball")
 func _process(delta):
-	print(meter.modulate.a)
+
 	if not basketball.is_shot and basketball.allow_input:
 		
-		if Input.is_action_pressed("Shoot"):
+		if Input.is_action_just_pressed("Shoot"):
 			meter.visible = true
+			$Timer.start()
 			if (meter.value == 100 or (meter.value == 0 and speed < 0)):
 				speed *= -1
 				transparency *= -1 
 				
-			meter.value += speed
-			meter.modulate.a -= transparency
+
+
 		if Input.is_action_just_released("Shoot"):
 			meter.value = 0
 			meter.modulate.a = 1
+
+
+func _on_timer_timeout():
+	meter.value += speed
+	meter.modulate.a -= transparency
+	if (meter.value == 100 or meter.value == 0):
+		speed *= -1
+		transparency *= -1 
+	$Timer.start()
